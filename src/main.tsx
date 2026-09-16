f47359b0cfcde221e86f0aee09d6006bb3ec8537
Louda <loudatuppal@gmail.com>
2026-09-16 22:06:20 +0800

    chore: establish app routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './index.css'
-import App from './App.tsx'
+
+import { HomeRoute } from './routes/home'
+import { NotFoundRoute } from './routes/not-found'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeRoute />} />
+        <Route path="*" element={<NotFoundRoute />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
