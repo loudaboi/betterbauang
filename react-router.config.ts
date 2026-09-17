85a4f6adb6e08ffc62e11e4b3c665a52d8041d3c
Louda <loudatuppal@gmail.com>
2026-09-17 09:19:14 +0800

    feat: prerender Bauang and barangay routes

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,18 @@
 import type { Config } from '@react-router/dev/config'
 
+import { getBarangays } from './src/lib/civic-data.server.ts'
+
 export default {
   appDirectory: 'src',
   ssr: false,
-  prerender: ['/'],
+  async prerender() {
+    const barangays = await getBarangays()
+
+    return [
+      '/',
+      '/bauang',
+      '/barangays',
+      ...barangays.map((barangay) => `/barangays/${barangay.slug}`),
+    ]
+  },
 } satisfies Config
