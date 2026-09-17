529f5f54e9588a808b8ead27e50b5885d7f89aa5
Louda <loudatuppal@gmail.com>
2026-09-17 09:19:17 +0800

    feat: prerender Bauang and barangay routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,5 +2,8 @@ import { index, route, type RouteConfig } from '@react-router/dev/routes'
 
 export default [
   index('./routes/home.tsx'),
+  route('bauang', './routes/bauang.tsx'),
+  route('barangays', './routes/barangays.tsx'),
+  route('barangays/:slug', './routes/barangay-detail.tsx'),
   route('*', './routes/not-found.tsx'),
 ] satisfies RouteConfig
