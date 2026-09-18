b896124cc032a530bd9f11ba9016ced300b3d7cb
Louda <loudatuppal@gmail.com>
2026-09-18 13:34:00 +0800

    chore: remove global search route

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,6 @@ export default [
   route('barangays/:slug', './routes/barangay-detail.tsx'),
   route('procurement', './routes/procurement.tsx'),
   route('procurement/:recordId', './routes/procurement-detail.tsx'),
-  route('search', './routes/search.tsx'),
   route('about', './routes/about.tsx'),
   route('about/sources', './routes/about-sources.tsx'),
   route('about/methodology', './routes/about-methodology.tsx'),
