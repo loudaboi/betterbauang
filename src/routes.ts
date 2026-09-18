915adba43fd9a86775c81e7b3668a4c48df5b46b
Louda <loudatuppal@gmail.com>
2026-09-18 14:21:53 +0800

    feat: register contact route

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { index, route, type RouteConfig } from '@react-router/dev/routes'
 
 export default [
   index('./routes/home.tsx'),
+  route('contact', './routes/contact.tsx'),
   route('bauang', './routes/bauang.tsx'),
   route('barangays', './routes/barangays.tsx'),
   route('barangays/:slug', './routes/barangay-detail.tsx'),
